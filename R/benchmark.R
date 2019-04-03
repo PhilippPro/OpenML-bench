6ba5bd602fd5c18c6787b1ad3c45f5de58f94ff1
PhilippPro <philipp_probst@gmx.de>
2019-04-03 13:32:13 +0200

    use rnw instead of tex. add tables and code

--- R/benchmark.R
+++ R/benchmark.R
@@ -11,7 +11,7 @@ tasks = listOMLTasks(tag = "OpenML-Reg19")
 ds = listOMLDataSets(tag = "OpenML-Reg19")
 
 lrns = list(makeLearner("regr.glmnet"), makeLearner("regr.rpart"), makeLearner("regr.kknn")) #, makeLearner("regr.svm"), makeLearner("regr.ranger"), makeLearner("regr.xgboost"))
-measures = measures = list(mse, mae, medse, medae, rsq, spearmanrho, kendalltau, timetrain)
+measures = measures = list(mse, mae, medse, medae, rsq, spearmanrho, kendalltau, timetrain, timepredict)
 
 bmr = list()
 for(i in c(1:nrow(tasks))[-28]) {
@@ -25,4 +25,49 @@ for(i in c(1:nrow(tasks))[-28]) {
   bmr[[i]] = benchmark(lrns, task, rin, measures = measures, keep.pred = TRUE, models = FALSE)
   save(bmr, file = "data/bmr.RData")
 }
+
+bmr
+
+# Analysis
+
+results = data.frame("task.id" = character(), "learner.id" = character(), "rsq.test.mean" = double(), "kendalltau.test.mean" = double(), "timetrain.test.mean" = double())
+
+for (i in c(1:length(bmr))[-28]){
+  resi = getBMRAggrPerformances(bmr[[i]], as.df = TRUE)
+  results = rbind(results, resi[, colnames(results)])
+}
+
+### Set Rsquared to 0 if smaller than 0 
+# results[results[, 3]<=0, 3] <- 0 
+
+### Set Zero if Kendalls Tau is NA
+# results[is.na(results[,4]) == TRUE,4] <- 0
+
+names(results)[2] <- "Learner"
+
+library(ggplot2)
+ggplot(data = results , aes(y = task.id, x = rsq.test.mean, group = Learner, color= Learner)) +
+  geom_point() +
+  lims(x=c(0,1))+
+  ylab( "Dataset") +
+  xlab("R-squared") #+
+  #theme(plot.margin = unit(c(5,0,5,0),"cm"))#
+
+library(ggplot2)
+ggplot(data = results , aes(y = task.id, x = kendalltau.test.mean, group = Learner, color= Learner)) +
+  geom_point() +
+  lims(x=c(0,1))+
+  ylab( "Dataset") +
+  xlab("Kendall's Tau") #+
+#theme(plot.margin = unit(c(5,0,5,0),"cm"))
+
+library(ggplot2)
+ggplot(data = results , aes(y = task.id, x = timetrain.test.mean, group = Learner, color= Learner)) +
+  geom_point() +
+  lims(x=c(0,1))+
+  ylab( "Dataset") +
+  xlab("Training time") #+
+#theme(plot.margin = unit(c(5,0,5,0),"cm"))
+
+# Grafik wie beim Vergleich der Tuningmethoden zur besseren Uebersicht
 
